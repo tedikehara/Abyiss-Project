97abfe6fbdb447751762193ee99d1f8ef89cd366
Theodore <tikehara@ucsc.edu>
2021-11-02 14:13:06 -0700

    some additional things

--- crypto_monitor.py
+++ crypto_monitor.py
@@ -13,19 +13,18 @@
 API_KEY = '''c1d3610a557a87beca88a20e781d37fa981a62c6658b46727b3cfe8a9c1927c9'''
 
 import cryptocompare as cc
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import folium
 
 # crypto compare object
 obj = cc.cryptocompare._set_api_key_parameter(API_KEY)
-# selenium driver
-driver = webdriver.Firefox()
 
-driver.get("https://www.google.com")
 
 # displays 10 crypto currencys and price in real time
-while True:
+i=0
+while i<5:
     print('BTC PRICE: ', cc.get_price(['BTC', 'ETH', 'BNB', 'USDT', 'ADA', 'SOL', 'XRP', 'DOT', 'SHIB', 'DOGE'], currency='USD'))
+    i+=1
+
 
 
 
